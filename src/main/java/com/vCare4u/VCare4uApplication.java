7febbd873c92068108f284536c27bba4c103a0c2
ManishKr-Git <manishkumarchoumiyan1998@gmail.com>
2021-05-05 16:52:21 +0530

    dofna

--- src/main/java/com/vCare4u/VCare4uApplication.java
+++ src/main/java/com/vCare4u/VCare4uApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VCare4uApplication {
-
-//	#
 	public static void main(String[] args) {
 		SpringApplication.run(VCare4uApplication.class, args);
 	}
