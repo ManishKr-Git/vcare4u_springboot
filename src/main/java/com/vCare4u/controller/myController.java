a6649e4eaa0787a391f1dc681e2e9e50c7f0426d
ManishKr-Git <manishkumarchoumiyan1998@gmail.com>
2021-07-22 22:37:39 +0530

    Commit

--- src/main/java/com/vCare4u/controller/myController.java
+++ src/main/java/com/vCare4u/controller/myController.java
@@ -25,8 +25,8 @@ import com.vCare4u.Entity.Reviews;
 import com.vCare4u.Entity.User;
 import com.vCare4u.services.ExpertServices;
 import com.vCare4u.services.UserServices;
-//@CrossOrigin("https://vcare4u-uoh.herokuapp.com")
-@CrossOrigin("http://localhost:3000")
+@CrossOrigin("http://vcare4u-uoh.herokuapp.com")
+//@CrossOrigin("http://localhost:3000")
 @RestController
 public class myController {
 	@Autowired
